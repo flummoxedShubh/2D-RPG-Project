fe19487f490276048cbc555336fbcc8e8b93333c
Shubh Sharma <flummoxedshubh@gmail.com>
2020-12-31 18:06:21 +0530

    Added a Character class

---
+++ 2D-RPG/Assets/Scripts/MonoBehaviours/Character.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//abstract identifier to indicate that this class can't be instantiated & must be inherited
+public abstract class Character : MonoBehaviour
+{
+    public int hitPoints;
+    public int maxHitPoints;
+}
