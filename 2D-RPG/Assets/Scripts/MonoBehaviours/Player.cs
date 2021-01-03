016c4c5bc20fa9dda2f5085ee227877e8fa810b4
Shubh Sharma <flummoxedshubh@gmail.com>
2021-01-03 22:14:09 +0530

    Update Player.cs

--- 2D-RPG/Assets/Scripts/MonoBehaviours/Player.cs
+++ 2D-RPG/Assets/Scripts/MonoBehaviours/Player.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player : Character
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+
         if(hitObject != null)
         {
             print("Hit: " + hitObject.objectName);
