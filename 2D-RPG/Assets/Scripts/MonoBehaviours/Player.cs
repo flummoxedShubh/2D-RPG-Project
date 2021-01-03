0cfe198e905a79d1d29818a312a39e4834ae811e
Shubh Sharma <flummoxedshubh@gmail.com>
2021-01-03 22:04:53 +0530

    Scriptable Object added

--- 2D-RPG/Assets/Scripts/MonoBehaviours/Player.cs
+++ 2D-RPG/Assets/Scripts/MonoBehaviours/Player.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Player : Character
 {
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+        if(hitObject != null)
+        {
+            print("Hit: " + hitObject.objectName);
+            collision.gameObject.SetActive(false);
+        }
+    }
 }
